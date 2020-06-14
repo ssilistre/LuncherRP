333e7583d5d49a27c5a00eaa2b5946fddf6497fb
SSilistre <24512383+ssilistre@users.noreply.github.com>
2020-06-14 10:21:11 +0300

    new windows form
    new window form

---
+++ LuancherFivemcode/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace LuancherFivemcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblOyna = new System.Windows.Forms.PictureBox();
+            this.lblKucult = new System.Windows.Forms.PictureBox();
+            this.lblKapat = new System.Windows.Forms.PictureBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.btndiscord = new System.Windows.Forms.PictureBox();
+            this.btnts3 = new System.Windows.Forms.PictureBox();
+            this.lblDuyuru = new System.Windows.Forms.Label();
+            this.lblKisiSayisi = new System.Windows.Forms.Label();
+            this.lblonline = new System.Windows.Forms.Label();
+            this.lblSunucuDurumu = new System.Windows.Forms.Label();
+            this.sunucuDurum = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lblOyna)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblKucult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblKapat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btndiscord)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnts3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sunucuDurum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblOyna
+            // 
+            this.lblOyna.BackColor = System.Drawing.Color.Transparent;
+            this.lblOyna.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblOyna.Image = ((System.Drawing.Image)(resources.GetObject("lblOyna.Image")));
+            this.lblOyna.Location = new System.Drawing.Point(827, 498);
+            this.lblOyna.Name = "lblOyna";
+            this.lblOyna.Size = new System.Drawing.Size(116, 32);
+            this.lblOyna.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.lblOyna.TabIndex = 0;
+            this.lblOyna.TabStop = false;
+            this.lblOyna.Click += new System.EventHandler(this.lblOyna_Click);
+            // 
+            // lblKucult
+            // 
+            this.lblKucult.BackColor = System.Drawing.Color.Transparent;
+            this.lblKucult.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblKucult.Image = ((System.Drawing.Image)(resources.GetObject("lblKucult.Image")));
+            this.lblKucult.Location = new System.Drawing.Point(861, 12);
+            this.lblKucult.Name = "lblKucult";
+            this.lblKucult.Size = new System.Drawing.Size(37, 36);
+            this.lblKucult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.lblKucult.TabIndex = 1;
+            this.lblKucult.TabStop = false;
+            this.lblKucult.Click += new System.EventHandler(this.lblKucult_Click);
+            // 
+            // lblKapat
+            // 
+            this.lblKapat.BackColor = System.Drawing.Color.Transparent;
+            this.lblKapat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblKapat.Image = ((System.Drawing.Image)(resources.GetObject("lblKapat.Image")));
+            this.lblKapat.Location = new System.Drawing.Point(906, 12);
+            this.lblKapat.Name = "lblKapat";
+            this.lblKapat.Size = new System.Drawing.Size(37, 36);
+            this.lblKapat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.lblKapat.TabIndex = 2;
+            this.lblKapat.TabStop = false;
+            this.lblKapat.Click += new System.EventHandler(this.lblKapat_Click);
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(406, 86);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(269, 49);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.logo.TabIndex = 3;
+            this.logo.TabStop = false;
+            // 
+            // btndiscord
+            // 
+            this.btndiscord.BackColor = System.Drawing.Color.Transparent;
+            this.btndiscord.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndiscord.Image = ((System.Drawing.Image)(resources.GetObject("btndiscord.Image")));
+            this.btndiscord.Location = new System.Drawing.Point(12, 498);
+            this.btndiscord.Name = "btndiscord";
+            this.btndiscord.Size = new System.Drawing.Size(116, 32);
+            this.btndiscord.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btndiscord.TabIndex = 4;
+            this.btndiscord.TabStop = false;
+            this.btndiscord.Click += new System.EventHandler(this.btndiscord_Click);
+            // 
+            // btnts3
+            // 
+            this.btnts3.BackColor = System.Drawing.Color.Transparent;
+            this.btnts3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnts3.Image = ((System.Drawing.Image)(resources.GetObject("btnts3.Image")));
+            this.btnts3.Location = new System.Drawing.Point(134, 498);
+            this.btnts3.Name = "btnts3";
+            this.btnts3.Size = new System.Drawing.Size(116, 32);
+            this.btnts3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnts3.TabIndex = 5;
+            this.btnts3.TabStop = false;
+            this.btnts3.Click += new System.EventHandler(this.btnts3_Click);
+            // 
+            // lblDuyuru
+            // 
+            this.lblDuyuru.BackColor = System.Drawing.Color.Transparent;
+            this.lblDuyuru.Location = new System.Drawing.Point(406, 138);
+            this.lblDuyuru.Name = "lblDuyuru";
+            this.lblDuyuru.Size = new System.Drawing.Size(272, 184);
+            this.lblDuyuru.TabIndex = 6;
+            this.lblDuyuru.Text = "...";
+            // 
+            // lblKisiSayisi
+            // 
+            this.lblKisiSayisi.AutoSize = true;
+            this.lblKisiSayisi.BackColor = System.Drawing.Color.Transparent;
+            this.lblKisiSayisi.ForeColor = System.Drawing.Color.White;
+            this.lblKisiSayisi.Location = new System.Drawing.Point(672, 498);
+            this.lblKisiSayisi.Name = "lblKisiSayisi";
+            this.lblKisiSayisi.Size = new System.Drawing.Size(54, 13);
+            this.lblKisiSayisi.TabIndex = 7;
+            this.lblKisiSayisi.Text = "Kişi Saysı:";
+            // 
+            // lblonline
+            // 
+            this.lblonline.AutoSize = true;
+            this.lblonline.BackColor = System.Drawing.Color.Transparent;
+            this.lblonline.ForeColor = System.Drawing.Color.White;
+            this.lblonline.Location = new System.Drawing.Point(728, 498);
+            this.lblonline.Name = "lblonline";
+            this.lblonline.Size = new System.Drawing.Size(13, 13);
+            this.lblonline.TabIndex = 8;
+            this.lblonline.Text = "..";
+            // 
+            // lblSunucuDurumu
+            // 
+            this.lblSunucuDurumu.AutoSize = true;
+            this.lblSunucuDurumu.BackColor = System.Drawing.Color.Transparent;
+            this.lblSunucuDurumu.ForeColor = System.Drawing.Color.White;
+            this.lblSunucuDurumu.Location = new System.Drawing.Point(639, 520);
+            this.lblSunucuDurumu.Name = "lblSunucuDurumu";
+            this.lblSunucuDurumu.Size = new System.Drawing.Size(87, 13);
+            this.lblSunucuDurumu.TabIndex = 9;
+            this.lblSunucuDurumu.Text = "Sunucu Durumu:";
+            // 
+            // sunucuDurum
+            // 
+            this.sunucuDurum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.sunucuDurum.Location = new System.Drawing.Point(731, 520);
+            this.sunucuDurum.Name = "sunucuDurum";
+            this.sunucuDurum.Size = new System.Drawing.Size(21, 13);
+            this.sunucuDurum.TabIndex = 10;
+            this.sunucuDurum.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(955, 542);
+            this.Controls.Add(this.sunucuDurum);
+            this.Controls.Add(this.lblSunucuDurumu);
+            this.Controls.Add(this.lblonline);
+            this.Controls.Add(this.lblKisiSayisi);
+            this.Controls.Add(this.lblDuyuru);
+            this.Controls.Add(this.btnts3);
+            this.Controls.Add(this.btndiscord);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.lblKapat);
+            this.Controls.Add(this.lblKucult);
+            this.Controls.Add(this.lblOyna);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.lblOyna)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblKucult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblKapat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btndiscord)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnts3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sunucuDurum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox lblOyna;
+        private System.Windows.Forms.PictureBox lblKucult;
+        private System.Windows.Forms.PictureBox lblKapat;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.PictureBox btndiscord;
+        private System.Windows.Forms.PictureBox btnts3;
+        private System.Windows.Forms.Label lblDuyuru;
+        private System.Windows.Forms.Label lblKisiSayisi;
+        private System.Windows.Forms.Label lblonline;
+        private System.Windows.Forms.Label lblSunucuDurumu;
+        private System.Windows.Forms.PictureBox sunucuDurum;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,106 @@
+# LuncherRP
+Open Source Fivem Luncher Library
+ 
+Yeni Tanıtım Videosu: 
+https://www.youtube.com/watch?v=xSYMGZG4HPg
+
+Eski Youtube anlatımı için: 
+https://youtu.be/5Ccpd3Cq-No
+
+## Sunucu Kayıt için = http://panel.fivemcode.com
+![Resim1](https://i.hizliresim.com/7091fF.png)
+
+  
+Selam arkadaşlar ücretsiz açık kaynaklı bir luncher geliştirdim. Bunuda minik bir kütüphane ile bağladım.  Minik kütüphaneyide web panele bağladım fivemcode.com ortaya çıktı.
+İsteyen arkadaşlar ücretsiz bir şekilde istedikleri tasarımı giydirerek üzerinde düzenleme yapabilir.  
+  
+**Bir takım özellikler;**  
+-Steam açık mı değil mi kontrol ediyor.  
+-Luncherı kapattığınızda oyunda kapanıyor.  
+-Basit sürekli güncellenen hile koruma algoritması mevcut.  
+-Luncher içine link ekleyebiliyorsunuz sınırsız.  
+-Discord üstünden mesaj gönderebilme
+-Online sunucudaki üyelerin sayısını görebilme
+-Lunchersız girişi kapatabilme
+-Whitelist özelliği ile üyeleri yönetebilme
+-Web panelden güncelleme yayınlama
+-Web panelden duyurular yayınlama
+-Otomatik steam id ve steam adını alır uygulama
+
+  
+  
+Projeyi github adresimden indirip üzerinde düzenleme yapabilirsiniz(Bu yol ile çok hızlı ilerleme kaydedebilirsiniz). Birden fazla kişi aynı özelliği isterse ekleyebilirim.  
+Eğer projeyi indirip düzenlerseniz aşağıdaki 3 adımı yapmanıza gerek yok.  
+  
+**1-Adım** = Yeni bir proje oluşturduktan sonra fivemluncher.dll projeye bileşen olarak eklemelisiniz.  
+   ![Adim1](https://s6.gifyu.com/images/adim1.gif)
+  
+**Adım 2** = Aşağıda ki kodu form.cs eklemelisiniz.  
+  
+Kopyala yapıştır yapmak için :  
+**lib rp = new lib();**  
+  
+**Adım 3** = Usingler kımına aşağıdaki kodu eklemek:  
+using fivemLuncher;  
+
+![adim3](https://i.hizliresim.com/phPeH1.png)
+
+  
+  
+Evet şimdi ne yapmak istiyorsanız luncherda onla alakalı komutu çağırabilirsiniz tüm komutlar aşağıda açıklamaları ile verilmiştir.  
+  
+**Kullanabileceğiniz komutlar:**  
+
+ - rp.serverinfos("serverkey") //Bu size özel sunucu keyinizle bilgilerinizi çeken komut.
+ - rp.takeplayerlist(serverkey) //bu komut oyuncu listesini indirmeye yarar.
+ - rp.kisiSayisi;// (Bu string değerdir online kişi sayısı bilgini barındırır.)
+ - rp.sunucuDurum;// (Bu string değer sunucunuzun durumunu barındırır.)
+ - rp.DiscordRC("ilk mesaj" , "ikinci mesaj"); //Bu aşağıdaki discord rc görünümün ilk satırı ve ikinci satırıdır.
+ - rp.stopHacks(); //Hile algoritmasını çalıştırır. 1 kere çalıştırır. Sürekli olması için timer içine eklemelisiniz.
+ - rp.ConnectWithPanel("serverkey"); //Bu komut ise luancher kontrolü yapar 
+ - rp.BackupSystem("ipadres","portno"); // Bu panel çalışmaz ise sizin sunucunuza bağlanmanızı sağlar.
+ - rp.ConnectFivem("ipadres" , "port numarası"); //normal bağlantı komutu
+ - rp.Cachedelete(); //Cache dosyalarını temizleyen komut.
+ - rp.discordopen("davetlinki")  //discordu açar ve panelden değiştirebilmenize yarar.
+ - rp.ts3open("ipadresi","port") //ts3 açar ve panelden değiştirebilmenize yarar.
+ - rp.closeFivem();  //Fivemi kapatır bu komut.
+ - rp.open("discord"); //discord açar ts açar artık ne istiyorsanız onu açar bu komut.
+ - rp.PlaySound("müzik.mp4"); //müzik mp4 adındaki şarkıyı çalar bu komut.
+ - rp.update(); // güncelleme komutudur.
+ - rp.duyurular; //(Bu komut string olarak duyurular yazısını barındırır.)
+ - rp.steamidgirformu(); //(Bu komut steamid girme formunu açtırır)
+ - rp.steamid64("buraya steam 64 idsi"); //Bu alana steam 64 idi vererek steamhex idisini alabilirsiniz.
+
+Hata Mesajlarını değiştirmek için yazılması gerekenler:
+
+            Lang.steamhata = "Lütfen Önce Steam Uygulamasını Başlatın"; //Please firt of open steam.
+            Lang.hatalimesajbaslik = "Dikkat dikkat !"; //Error Header Message
+            Lang.muzikhata = "Müzik Dosyası Bulunamadı"; //Play Music Not Found Error.
+            Lang.panelhata = "Panel Sistemi ile iletişime geçilemedi !"; //Panel System ERROR
+            Lang.steam64idgirinhatasi = "Lütfen Sadece Steam64 idsini giriniz."; // Steamid errorr
+            Lang.cokfazlaistekhatasi = "Çok fazla istek gönderildi."; //To Much Post Request error.
+            Lang.uygulamaacmahatasi = "Açmaya çalıştığınız şeyi bulamadık."; //Open Command error
+            Lang.internethatasi = "İnternet bağlantınız bulunamadı."; //Internet Connection Errror.
+
+ 
+
+ Özel olarak discord sunucumdan bana ulaşabilirsiniz : https://discord.gg/tD5AXcA
+
+**Discord RC Görünümü:**
+  
+![RC](https://i.hizliresim.com/cioI2L.png)
+
+
+
+
+### Sunucuyu buradan kayit ettirebilirsiniz https://panel.fivemcode.com
+ 
+ En sonunda sunucu dosyalarını indirin sunucu dosyalarının içinde ki server.lua aşağıdaki resimdeki alanları düzenleyin.Altı kırmızı çizili alana sunucu kayit daki özel idnizi giriniz.
+ ![Lua görüntüsü](https://i.hizliresim.com/HARCP9.png)
+ 
+ **Sunucu dosyalari için** https://github.com/ssilistre/LuncherRP/releases/download/V1.0.0.7/yeni_Sunucu_Dosyalari.rar
+
+Sorunlar olabilir bana yazarak iletebilirsiniz. Eklenmesini istediğiniz şeyler olabilir onları da iletebilirsiniz. Ücretsiz arkadaşlar üstüne sizde geliştirme ekleyebilirsiniz.  
+Yukarıdaki komutları kullanarak kendi tasarımızı da geliştirmeniz mümkün. Burada ki hile algoritması %100 sonuç vermeyebilir , engelleyecektir. Minik bir denemeyi cheatengine ile yapabilirsiniz.  
+  
+**Github projesi linki** : [https://github.com/ssilistre/LuncherRP](https://github.com/ssilistre/LuncherRP)
